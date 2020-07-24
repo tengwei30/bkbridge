34c8479777200e356dd0a3eab379d03d5f7f3869
zhangsong <zhangsong@guanghe.tv>
2020-07-24 11:17:59 +0800

    feat: 更名

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+// const webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'BKWebViewBridge.js',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    library: 'BKWebViewBridge',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  devtool: 'source-map',
+  target: 'web',
+  optimization: {
+    minimizer: [
+      // 压缩JS
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_debugger: true, // 去除debugger
+            drop_console: true, // 去除console.log
+          },
+          warnings: false,
+        },
+        cache: true, // 开启缓存
+        parallel: true, // 平行压缩
+      }),
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+}
